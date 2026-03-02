a2724cade005cf73032c2bc4d7e1619ea25c0d76
hanifjamadar77 <jamadarmhanif@gmail.com>
2026-03-02 17:48:41 +0530

    logout problem solved

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 import { account } from "../services/appwrite";
-import { View, ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
   const [user, setUser] = useState<any>(null);
