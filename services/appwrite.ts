e4959cca46bdb149a4b88efd46f0ce6e316dac23
hanifjamadar77 <jamadarmhanif@gmail.com>
2026-03-02 16:52:02 +0530

    changing the endpoint

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Storage } from "react-native-appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
   .setProject("6988c3bb0022f94e6b3d")
   .setPlatform("com.hanif_jamadar.worktrack");
 
